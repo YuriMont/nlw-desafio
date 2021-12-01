50f9d9ab12a6351982e9e00ae70117dae9f40e06
YuriMont <yuri.mont1043@gmail.com>
2021-12-01 19:57:12 -0300

    Desafio finalizado

---
+++ main.js
@@ -0,0 +1,29 @@
+const linksSocialMedia={
+    github: "YuriMont",
+    youtube: '',
+    facebook: '',
+    instagram: "yuri.monteiro1043",
+    twitter: ''
+}
+
+function changeSocialMediaLinks(){
+    for(let li of socialLinks.children){
+        const socialMedia = li.getAttribute('class');
+        li.children[0].href = `https://${socialMedia}.com/${linksSocialMedia[socialMedia]}/`;
+    }
+}
+
+function getGitHubProfileInfos(){
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('github').textContent=linksSocialMedia.github,
+        document.getElementById('name').textContent=data.name,
+        document.getElementById('bio').textContent=data.bio,
+        document.getElementById('profileImage').src=data.avatar_url
+    });
+}
+
+getGitHubProfileInfos();
+changeSocialMediaLinks();
